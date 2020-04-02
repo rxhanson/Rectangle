430b2b1d91ad14a690e3bc7d1f36eebcb0f46f3e
Ryan Hanson <ryanhansonr@gmail.com>
2020-04-02 22:51:57 +0200

    Initialized Rectangle as the frontmost app upon launch

--- Rectangle/ApplicationToggle.swift
+++ Rectangle/ApplicationToggle.swift
@@ -11,8 +11,8 @@ import Cocoa
 class ApplicationToggle: NSObject {
     
     private var disabledApps = Set<String>()
-    public private(set) var frontAppId: String?
-    public private(set) var frontAppName: String?
+    public private(set) var frontAppId: String? = "com.knollsoft.Rectangle"
+    public private(set) var frontAppName: String? = "Rectangle"
     public private(set) var shortcutsDisabled: Bool = false
     
     private let shortcutManager: ShortcutManager
