431e3b34e662352fa8536bb4e913f344d7e2c75f
Ryan Hanson <ryanhansonr@gmail.com>
2020-09-16 19:40:09 +0200

    Align stack views, initial calculations for sixths

--- Rectangle/WindowCalculation/TopCenterSixthCalculation.swift
+++ Rectangle/WindowCalculation/TopCenterSixthCalculation.swift
@@ -9,5 +9,68 @@
 import Foundation
 
 class TopCenterSixthCalculation: WindowCalculation {
+    //    private var centerThirdCalculation: CenterThirdCalculation?
+    //    private var lastThirdCalculation: LastThirdCalculation?
+    //
+    //    init(repeatable: Bool = true) {
+    //        if repeatable && Defaults.subsequentExecutionMode.value != .none {
+    //            centerThirdCalculation = CenterThirdCalculation()
+    //            lastThirdCalculation = LastThirdCalculation(repeatable: false)
+    //        }
+    //    }
     
+    override func calculateRect(_ window: Window, lastAction: RectangleAction?, visibleFrameOfScreen: CGRect, action: WindowAction) -> RectResult {
+        guard Defaults.subsequentExecutionMode.value != .none,
+            let last = lastAction, let lastSubAction = last.subAction else {
+                return sixthRect(window, lastAction: lastAction, visibleFrameOfScreen: visibleFrameOfScreen, action: action)
+        }
+        
+        //        var calculation: WindowCalculation?
+        //
+        //        if last.action == .firstThird {
+        //            switch lastSubAction {
+        //            case .topThird, .leftThird:
+        //                calculation = centerThirdCalculation
+        //            case .centerHorizontalThird, .centerVerticalThird:
+        //                calculation = lastThirdCalculation
+        //            default:
+        //                break
+        //            }
+        //        } else if last.action == .lastThird {
+        //            switch lastSubAction {
+        //            case .topThird, .leftThird:
+        //                calculation = centerThirdCalculation
+        //            default:
+        //                break
+        //            }
+        //        }
+        //
+        //        if let calculation = calculation {
+        //            return calculation.calculateRect(window, lastAction: lastAction, visibleFrameOfScreen: visibleFrameOfScreen, action: action)
+        //        }
+        
+        return sixthRect(window, lastAction: lastAction, visibleFrameOfScreen: visibleFrameOfScreen, action: action)
+    }
+    
+    func sixthRect(_ window: Window, lastAction: RectangleAction?, visibleFrameOfScreen: CGRect, action: WindowAction) -> RectResult {
+        return isLandscape(visibleFrameOfScreen)
+            ? RectResult(landscapeSixth(visibleFrameOfScreen), subAction: .leftThird)
+            : RectResult(portraitSixth(visibleFrameOfScreen), subAction: .topThird)
+    }
+    
+    private func landscapeSixth(_ visibleFrameOfScreen: CGRect) -> CGRect {
+        var rect = visibleFrameOfScreen
+        rect.size.width = floor(visibleFrameOfScreen.width / 3.0)
+        rect.size.height = floor(visibleFrameOfScreen.height / 2.0)
+        rect.origin.x = visibleFrameOfScreen.origin.x + rect.width
+        rect.origin.y = visibleFrameOfScreen.origin.y + rect.height
+        return rect
+    }
+    
+    private func portraitSixth(_ visibleFrameOfScreen: CGRect) -> CGRect {
+        var rect = visibleFrameOfScreen
+        //            rect.size.height = floor(visibleFrameOfScreen.height / 4.0)
+        //            rect.origin.y = visibleFrameOfScreen.origin.y + visibleFrameOfScreen.height - (rect.height * 2.0)
+        return rect
+    }
 }
