38fbb74ab6cc56533d1d032bb47eda27dc2ede95
decodism <77468771+decodism@users.noreply.github.com>
2022-08-04 02:10:26 +0200

    Stage Manager: only one window case

--- Rectangle/Utilities/StageUtil.swift
+++ Rectangle/Utilities/StageUtil.swift
@@ -14,5 +14,23 @@ class StageUtil {
         
         return stageDefaults.bool(forKey: "GloballyEnabled") && !stageDefaults.bool(forKey: "AutoHide")
     }
+
+    static func stageWindowPresent() -> Bool {
+        let options = CGWindowListOption(arrayLiteral: .excludeDesktopElements, .optionOnScreenOnly)
+        if let windowInfo = CGWindowListCopyWindowInfo(options, 0) as? Array<Dictionary<String,Any>> {
+            for infoDict in windowInfo {
+                if let bounds = infoDict[kCGWindowBounds as String] as? [String: CGFloat] {
+                    guard let name = infoDict[kCGWindowOwnerName as String] as? String,
+                          let w = bounds["Width"],
+                          let h = bounds["Height"]
+                    else { continue }
+                    if name == "WindowManager" && w == 64 && h == 64 {
+                        return true
+                    }
+                }
+            }
+        }
+        return false
+    }
     
 }
