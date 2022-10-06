0d997376781a7916fd71bdab60d68bcecd4cf1fa
Ryan Hanson <ryan@ryanhanson.dev>
2022-10-06 11:29:25 -0400

    Update for Ventura sys prefs rename

--- Rectangle/AccessibilityAuthorization/AccessibilityWindowController.swift
+++ Rectangle/AccessibilityAuthorization/AccessibilityWindowController.swift
@@ -25,6 +25,20 @@ class AccessibilityWindowController: NSWindowController {
 
 class AccessibilityViewController: NSViewController {
     
+    @IBOutlet weak var sysPrefsPathField: NSTextField!
+    @IBOutlet weak var openSysPrefsButton: NSButton!
+    @IBOutlet weak var padlockField: NSTextField!
+    
+    override func viewDidLoad() {
+        if #available(OSX 13, *) {
+            sysPrefsPathField.stringValue =  NSLocalizedString(
+                "Go to System Settings → Privacy & Security → Accessibility", tableName: "Main", value: "", comment: "")
+            openSysPrefsButton.title = NSLocalizedString(
+                "Open System Settings", tableName: "Main", value: "", comment: "")
+            padlockField.isHidden = true
+        }
+    }
+    
     @IBAction func openSystemPrefs(_ sender: Any) {
         NSWorkspace.shared.open(URL(string:"x-apple.systempreferences:com.apple.preference.security?Privacy_Accessibility")!)
     }
