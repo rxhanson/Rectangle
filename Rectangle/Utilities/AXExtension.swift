47814123675fe3ed3e8a487cec18542210cefcba
decodism <decodism@protonmail.com>
2022-11-22 14:18:10 +0100

    Fix memory leak in AXValue

--- Rectangle/Utilities/AXExtension.swift
+++ Rectangle/Utilities/AXExtension.swift
@@ -16,13 +16,14 @@ extension AXValue {
     func toValue<T>() -> T? {
         let pointer = UnsafeMutablePointer<T>.allocate(capacity: 1)
         let success = AXValueGetValue(self, AXValueGetType(self), pointer)
-        return success ? pointer.pointee : nil
+        let value = pointer.pointee
+        pointer.deallocate()
+        return success ? value : nil
     }
     
     static func from<T>(value: T, type: AXValueType) -> AXValue? {
-        let pointer = UnsafeMutablePointer<T>.allocate(capacity: 1)
-        pointer.pointee = value
-        return AXValueCreate(type, pointer)
+        var value = value
+        return AXValueCreate(type, &value)
     }
 }
 
