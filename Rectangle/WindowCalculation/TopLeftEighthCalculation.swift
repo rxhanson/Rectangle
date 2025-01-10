6b04425ac22f884e36f313120470f055bf762baf
Joaquin Mendez <38924946+joaquin-splunk@users.noreply.github.com>
2025-01-09 19:57:39 -0800

    Invert calculation for TopLeftEighthCalculation on portrait mode

--- Rectangle/WindowCalculation/TopLeftEighthCalculation.swift
+++ Rectangle/WindowCalculation/TopLeftEighthCalculation.swift
@@ -42,8 +42,8 @@ class TopLeftEighthCalculation: WindowCalculation, OrientationAware, EighthsRepe
     
     func portraitRect(_ visibleFrameOfScreen: CGRect) -> RectResult {
         var rect = visibleFrameOfScreen
-        rect.size.width = floor(visibleFrameOfScreen.width / 4.0)
-        rect.size.height = floor(visibleFrameOfScreen.height / 2.0)
+        rect.size.width = floor(visibleFrameOfScreen.width / 2.0)
+        rect.size.height = floor(visibleFrameOfScreen.height / 4.0)
         rect.origin.y = visibleFrameOfScreen.maxY - rect.height
         rect.origin.x = visibleFrameOfScreen.minX
         return RectResult(rect, subAction: .topLeftEighth)
