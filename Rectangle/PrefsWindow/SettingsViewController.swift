066bc413f189b1c5ed2b729033d9c59f62f7e515
Arham Wani <arhamwani765@gmail.com>
2026-08-07 05:50:00 +0530

    Reapply restored shortcut defaults

--- Rectangle/PrefsWindow/SettingsViewController.swift
+++ Rectangle/PrefsWindow/SettingsViewController.swift
@@ -252,17 +252,20 @@ class SettingsViewController: NSViewController {
         if response == .alertThirdButtonReturn { return }
 
         //  Restore default shortcuts
-        WindowAction.active.forEach { UserDefaults.standard.removeObject(forKey: $0.name) }
         let rectangleDefaults = response == .alertFirstButtonReturn
-        if rectangleDefaults != Defaults.alternateDefaultShortcuts.enabled {
-            Defaults.alternateDefaultShortcuts.enabled = rectangleDefaults
-            Notification.Name.changeDefaults.post()
-        }
+        Self.restoreShortcutDefaults(rectangleDefaults: rectangleDefaults)
         
         // Restore snap areas
         Defaults.portraitSnapAreas.typedValue = nil
         Defaults.landscapeSnapAreas.typedValue = nil
         Notification.Name.defaultSnapAreas.post()
+    }
+
+    static func restoreShortcutDefaults(rectangleDefaults: Bool,
+                                        notificationCenter: NotificationCenter = .default) {
+        WindowAction.active.forEach { UserDefaults.standard.removeObject(forKey: $0.name) }
+        Defaults.alternateDefaultShortcuts.enabled = rectangleDefaults
+        Notification.Name.changeDefaults.post(center: notificationCenter)
     }
     
     @IBAction func exportConfig(_ sender: NSButton) {
