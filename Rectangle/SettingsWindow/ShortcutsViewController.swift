5702eded8acf1f2e8c8d60f240003549542f3e46
Ryan Hanson <ryan@ryanhanson.dev>
2026-09-24 11:48:49 -0400

    Removed old storyboard items and corresponding appkit files

--- Rectangle/SettingsWindow/ShortcutsViewController.swift
+++ Rectangle/SettingsWindow/ShortcutsViewController.swift
@@ -447,3 +447,19 @@ extension ShortcutsViewController: NSOutlineViewDelegate {
         }
     }
 }
+
+class PassthroughShortcutValidator: MASShortcutValidator {
+    
+    override func isShortcutValid(_ shortcut: MASShortcut!) -> Bool {
+        return true
+    }
+    
+    override func isShortcutAlreadyTaken(bySystem shortcut: MASShortcut!, explanation: AutoreleasingUnsafeMutablePointer<NSString?>!) -> Bool {
+        return false
+    }
+    
+    override func isShortcut(_ shortcut: MASShortcut!, alreadyTakenIn menu: NSMenu!, explanation: AutoreleasingUnsafeMutablePointer<NSString?>!) -> Bool {
+        return false
+    }
+    
+}
